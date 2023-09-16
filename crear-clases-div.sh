e650af73ee580c5f0041e88b6c646776ae6ea40a
NEBULA1978 <rpascualey@gmail.com>
2023-09-17 00:56:23 +0200

    Script para crear clases de 1 al nº le diga falta mejorar

---
+++ crear-clases-div.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Pregunta al usuario cuántos elementos desea agregar (empezando desde 1)
+read -p "¿Cuántos elementos deseas agregar (empezando desde 1)? " num_elements
+
+# Verifica que el número sea válido
+if [ $num_elements -ge 0 ]; then
+  # Abre el archivo HTML para agregar elementos
+  echo "<!-- Elementos agregados automáticamente -->" > temp.html
+  echo "        <div class=\"grid-container\">" >> temp.html
+  for ((i = 1; i <= num_elements; i++)); do
+    echo "            <div class=\"item$i\">$i</div>" >> temp.html
+  done
+  echo "        </div>" >> temp.html
+
+  # Combina el archivo temporal con el archivo original y guarda la salida en "grid-container-hijos-modificado.html"
+  cat grid-container-hijos.html temp.html > grid-container-hijos-modificado.html
+
+  # Limpia el archivo temporal
+  rm temp.html
+
+  echo "Se han agregado $num_elements elementos al archivo 'grid-container-hijos-modificado.html' dentro del contenedor existente."
+else
+  echo "Número inválido. Debes agregar al menos un elemento."
+fi
